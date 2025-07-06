4b7650b74f5d8f2a9fb4a8390814d67bf63c0b98
shamsheedali <shamsheedhopz0786@gmail.com>
2025-07-06 10:51:36 +0530

    fix: Footer Social Media Icons

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,36 @@
 import layoutBg from "@/assets/images/common/layout-bg.png";
+import logo from "@/assets/images/common/logo.png";
+import type { IconName } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
+  const socialIcons: [IconName, string][] = [
+    ["facebook-f", "Facebook"],
+    ["twitter", "Twitter"],
+    ["instagram", "Instagram"],
+    ["linkedin-in", "LinkedIn"],
+    ["youtube", "YouTube"],
+  ];
   return (
     <footer
-      className="h-[130px] w-full bg-cover absolute bottom-0"
+      className="h-[130px] w-full bg-cover absolute bottom-0 flex flex-col gap-2 justify-center items-center"
       style={{ backgroundImage: `url(${layoutBg})` }}
-    ></footer>
+    >
+      <div className="flex flex-col items-center gap-2">
+        <img src={logo} alt="logo" className="w-[100px]" />
+
+        <ul className="text-white flex gap-3 text-lg">
+          {socialIcons.map(([icon], idx) => (
+            <li
+              key={idx}
+              className="w-10 aspect-square border-2 cursor-pointer border-white rounded-full flex items-center justify-center"
+            >
+              <FontAwesomeIcon icon={["fab", icon as IconName]} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   );
 };
 
